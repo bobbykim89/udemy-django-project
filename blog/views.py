fc939076f131819ac5d3a780d98d407919df9e4e
Sihun Kim <skim585@asu.edu>
2024-10-12 23:34:18 -0700

    feat(*): initial commit

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def starting_page(req):
+    return render(req, 'blog/index.html')
+
+
+def posts(req):
+    pass
+
+
+def post_detail(req):
+    pass
