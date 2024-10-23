f6180b1aa1aeba9c42230fa98f2afc0aa7d85e5c
Sihun Kim <skim585@asu.edu>
2024-10-22 22:34:59 -0700

    feat(blog/*): update models to upload image files in admin panel, updated to class based views
    update models to upload images in admin panel instead of adding image file names. update views to use class based view. update templates accordingly.

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', view=views.starting_page, name='starting-page'),
-    path("posts", view=views.posts, name='posts-page'),
-    path('posts/<slug:slug>', view=views.post_detail,
+    path('', view=views.StartingPageView.as_view(), name='starting-page'),
+    path("posts", view=views.AllPostsView.as_view(), name='posts-page'),
+    path('posts/<slug:slug>', view=views.SinglPostView.as_view(),
          name='post-detail-page')  # /posts/my-post
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,56 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render, get_object_or_404
-from datetime import date
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
 # Create your views here.
 
+class StartingPageView(ListView):
+    template_name = 'blog/index.html'
+    model = Post
+    ordering = ['-date']
+    context_object_name = 'posts'
 
-def starting_page(req):
-    latest_posts = Post.objects.all().order_by('-date')[:3]
-    return render(req, 'blog/index.html', context={
-        'posts': latest_posts
-    })
+    def get_queryset(self) -> QuerySet[Any]:
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+# def starting_page(req):
+#     latest_posts = Post.objects.all().order_by('-date')[:3]
+#     return render(req, 'blog/index.html', context={
+#         'posts': latest_posts
+#     })
 
 
-def posts(req):
-    all_posts = Post.objects.all().order_by('-date')
-    return render(req, 'blog/all-posts.html', context={
-        'all_posts': all_posts
-    })
+class AllPostsView(ListView):
+    template_name = 'blog/all-posts.html'
+    model = Post
+    ordering = ['-date']
+    context_object_name = 'all_posts'
 
 
-def post_detail(req, slug):
-    identified_post = get_object_or_404(Post, slug=slug)
-    return render(req, 'blog/post-detail.html', context={
-        'post': identified_post,
-        'post_tags': identified_post.tags.all()
-    })
+# def posts(req):
+#     all_posts = Post.objects.all().order_by('-date')
+#     return render(req, 'blog/all-posts.html', context={
+#         'all_posts': all_posts
+#     })
+
+class SinglPostView(DetailView):
+    template_name = 'blog/post-detail.html'
+    model = Post
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['post_tags'] = self.object.tags.all()
+        return context
+
+
+# def post_detail(req, slug):
+#     identified_post = get_object_or_404(Post, slug=slug)
+#     return render(req, 'blog/post-detail.html', context={
+#         'post': identified_post,
+#         'post_tags': identified_post.tags.all()
+#     })
